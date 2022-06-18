3a3bfc4774ddfa0cad7777ff20de83452e3676c4
Artem Paliutin <pal.artem@icloud.com>
2022-06-19 02:05:11 +0300

    Initial Commit

---
+++ CalcDemo/CalcDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcDemoApp.swift
+//  CalcDemo
+//
+//  Created by Artem Paliutin on 19/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct CalcDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
